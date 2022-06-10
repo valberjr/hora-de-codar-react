f73fe8a60767dc4083c6e015d1912951605607d5
valberjr <3cifhzht@duck.com>
2022-06-10 10:04:58 -0300

    passar eventos por props

--- fundamentos/meu-projeto/src/App.js
+++ fundamentos/meu-projeto/src/App.js
@@ -4,13 +4,10 @@ import Evento from './components/Evento';
 import Form from './components/Form';
 
 function App() {
-    const nome = 'Maria';
     return (
         <div className="App">
             <h1>Testando Eventos</h1>
             <Evento />
-            <Evento numero={1} />
-            <Evento numero={2} />
             <Form />
         </div>
     );
