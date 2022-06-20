bb3e7d89a0cb1c4501a5efcb30e1b60440468c46
valberjr <3cifhzht@duck.com>
2022-06-20 11:02:48 -0300

    criando dashboard

--- costs/src/pages/Projects.js
+++ costs/src/pages/Projects.js
@@ -1,6 +1,10 @@
 import { useLocation } from 'react-router-dom';
 
 import Message from '../components/layout/Message';
+import Container from '../components/layout/Container';
+import LinkButton from '../components/layout/LinkButton';
+
+import styles from './Projects.module.css';
 
 function Projects() {
     const location = useLocation();
@@ -12,9 +16,15 @@ function Projects() {
     }
 
     return (
-        <div>
-            <h1>Meus Projetos</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/newproject" text="Criar Projeto" />
+            </div>
             {message && <Message type="success" msg={message} />}
+            <Container customClass="start">
+                <p>Projetos...</p>
+            </Container>
         </div>
     );
 }
