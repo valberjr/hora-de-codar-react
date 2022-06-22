3f1a39cb0ed2d79505d2184d172b1ac7be389992
valberjr <3cifhzht@duck.com>
2022-06-22 09:02:40 -0300

    página de edição de dados

--- costs/src/App.js
+++ costs/src/App.js
@@ -5,6 +5,7 @@ import Contact from './components/pages/Contact';
 import Home from './components/pages/Home';
 import NewProject from './components/pages/NewProject';
 import Projects from './components/pages/Projects';
+import Project from './components/pages/Project';
 
 import Container from './components/layout/Container';
 import Footer from './components/layout/Footer';
@@ -21,6 +22,7 @@ function App() {
                     <Route path="/company" element={<Company />} />
                     <Route path="/contact" element={<Contact />} />
                     <Route path="/newproject" element={<NewProject />} />
+                    <Route path="/project/:id" element={<Project />} />
                 </Routes>
             </Container>
             <Footer />
